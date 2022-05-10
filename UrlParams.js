e39825b25b6ca7d43f2b86b63a4f48840c6e1647
Maikel Hendrikx <464318@student.fontys.nl>
2022-05-10 13:48:08 +0200

    Added comments to UrlParams.js

--- UrlParams.js
+++ UrlParams.js
@@ -1,8 +1,10 @@
 const querystring = window.location.search;
 const urlParams = new URLSearchParams(querystring);
-let urlDate = urlParams.get('date');
-let urlTime = urlParams.get('time');
 
+let urlDate = urlParams.get('date'); //gets the value behind ?date in the url
+let urlTime = urlParams.get('time'); //gets the value behind ?time in the url
+
+//if date was supplied in URL, load the data for that day. Otherwise get the current date.
 if (urlDate != null) {
     document.getElementById('dateNow').value = urlDate;
     getDataByDay(new Date(urlDate));
@@ -11,6 +13,7 @@ else {
     getDataByDay(new Date());
 }
 
+//if time was supplied in URL, load the data for that time. Otherwise get the current time.
 if (urlTime != null) {
     document.getElementById('slider').value = urlTime;
     document.getElementById('SelectValue').innerHTML = convertHMS(urlTime);
@@ -20,18 +23,21 @@ else {
     SelectValue.innerHTML = convertHMS(seconds);
 }
 
+//Generates the url that can be used to share the current date/time.
 function GenerateSharePopup() {
     let shareString = location.origin + `?date=${document.getElementById('dateNow').value}&time=${document.getElementById('slider').value}`;
 
     document.getElementById('datetimecopy').value = shareString;
 }
 
+//Copies the current url to the user's clipboard
 function CopyTo() {
     let copyText = document.getElementById('datetimecopy').value;
 
     navigator.clipboard.writeText(copyText);
 }
 
+//Updates the URL with the selected date/time without reloading the page.
 function updateUrlParams(){
     window.history.replaceState({}, null, `?date=${document.getElementById('dateNow').value}&time=${document.getElementById('slider').value}`);
 }
