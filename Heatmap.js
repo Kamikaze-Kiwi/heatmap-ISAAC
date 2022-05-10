249443c66f26f8f5e6b5136d3422fc701cef0f9a
Maikel Hendrikx <464318@student.fontys.nl>
2022-05-10 13:39:26 +0200

    Added comments to Heatmap.js

--- Heatmap.js
+++ Heatmap.js
@@ -5,13 +5,16 @@ let errorText = document.getElementById('alert-text');
 let errorDiv = document.getElementById('errorAlert');
 let spinner = document.getElementById('loadingSpinner');
 
+
+//Create a Highcharts chart on the div with the id 'container'.
 var chart = Highcharts.chart('container', {
     data: {
+        //adds default data to prevent highcharts from getting an error.
         rows: [['x', 'y', 'Temperature'],
         [0, 0, 0]]
     },
     chart: {
-        type: 'contour',
+        type: 'contour', //type: contour makes it so spots on the heatmap with no data will be interpolated.
         inverted: false,
         plotBackgroundImage: 'derde-verdieping.svg'
     },
@@ -27,7 +30,7 @@ var chart = Highcharts.chart('container', {
     },
     tooltip: {
         formatter: function () {
-            return this.point.value + '&nbsp;&#8451;';
+            return this.point.value + '&nbsp;&#8451;'; //tooltip shows it's value along with degrees celsius symbol.
         }
     },
     yAxis: {
@@ -63,7 +66,9 @@ var chart = Highcharts.chart('container', {
             [0.50, 'rgba(250,250,0,0.5)'],
             [0.80, 'rgba(250,0,0,0.5)']
         ],
-        min: 10,
+
+        //values below or above min/max will show the same color as if it were 10 or 40 respectively.
+        min: 10, 
         max: 40
     },
     series: [{
@@ -71,6 +76,8 @@ var chart = Highcharts.chart('container', {
     }]
 });
 
+
+//Retrieves the data for the supplied day from the API endpoint.
 function getDataByDay(datetime) {
     let config = {
         params: {
@@ -78,18 +85,21 @@ function getDataByDay(datetime) {
         }
     }
 
+    //shows a loading spinner
     spinner.style.display = 'block';
 
+    //performs an axios get request to get all data for the supplied day
     axios.get(
         'https://i447113.luna.fhict.nl/api/temperatures/History', config
     ).then(function (response) {
         selectedDayData = response.data
     }).catch(function (error) {
-        alert('Error loading data');
-        spinner.style.display = 'none';
+        alert('Error loading data'); //shows an error message
+        spinner.style.display = 'none'; //hides the loading spinner again
     }).finally(function () {
         let anyData = false;
 
+        //checks if there is any data for this day
         selectedDayData.forEach(element => {
             if (element.length > 0) {
                 errorDiv.style.display = 'none';
@@ -99,38 +109,53 @@ function getDataByDay(datetime) {
             }
         });
 
+        //if no data was found for this day, show an error message
         if (!anyData) {
             errorDiv.style.display = 'block';
             errorText.innerText = 'Er is geen data gevonden voor deze dag.';
             emptyToday = true;
         }
 
+        //updates the heatmap using the newly selected 
         updateHeatmapByTime(document.getElementById('slider').value / 30);
-        spinner.style.display = 'none';
 
+        //hides the loading spinner again
+        spinner.style.display = 'none';
     })
 }
 
+
+//updates the heatmap data for the current time.
+//index is the current time in seconds / 30.
 function updateHeatmapByTime(index) {
     let currentData = selectedDayData[index];
 
+    //parses the received data (JSON) to the rows data used by highcharts. 
     let parsedData = [['x', 'y', 'Temperature']];
     currentData.forEach(sensor => {
         parsedData.push([sensor.x, sensor.y, sensor.value])
     });
 
+    //if there is no data for this time, push empty data to the list to prevent highcharts from crashing.
     if (parsedData.length == 1) {
         parsedData.push([0, 0, 0]);
+
+        //if there is data on another time for this day, 
+        //display an error message which shows the user there is no data for the current moment.
+        //otherwise, the error message will still show that there is no data for the entire day.
         if (!emptyToday) {
             errorDiv.style.display = 'block';
             errorText.innerText = 'Er is geen data gevonden voor dit moment.';
         }
 
     }
+
+    //if there is data for this time, hide the error message.
     else {
         errorDiv.style.display = 'none';
     }
 
+    //update the chart with the new data.
     chart.series[0].update({
         data: parsedData
     })
