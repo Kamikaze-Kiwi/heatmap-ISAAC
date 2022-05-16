a34f2c6bf3282db7ae49c841cda3df75fff3952a
Maikel Hendrikx <464318@student.fontys.nl>
2022-05-16 14:46:21 +0200

    Added comments to floormanager

--- floorManager.js
+++ floorManager.js
@@ -13,35 +13,42 @@ let floors = [
     { floor: 6, src: 'derde-verdieping.svg' },
 ]
 
+//disable floor down button if already at bottom floor
 if (currentFloor <= minFloor){
     currentFloor = minFloor;
     buttonDown.disabled = true;
 }
 
+//disable floor up button if already at top floor
 if (currentFloor >= maxFloor) {
     currentFloor = maxFloor;
     buttonUp.disabled = true;
 }
 
+//Gets the floor for the specified floor number
 function GetFloor(floorNum){
     return floors.filter(function (x){
         return x.floor == floorNum;
     })[0]
 }
 
+//Goes up one floor
 function FloorUp(){
     floorSelect.selectedIndex += 1;
 
     SetFloor(floorSelect.value);
 }
 
+//Goes down one floor
 function FloorDown(){
     floorSelect.selectedIndex -= 1;
 
     SetFloor(floorSelect.value);
 }
 
+//Sets the floor to the specified floor
 function SetFloor(newFloor){
+    //disable/re-enable the up button depending on current floor.
     if (floorSelect.selectedIndex + 1 >= floorSelect.length) {
         buttonUp.disabled = true;
     }
@@ -49,6 +56,7 @@ function SetFloor(newFloor){
         buttonUp.disabled = false;
     }   
 
+    //disable/re-enable the down button depending on current floor.
     if (floorSelect.selectedIndex <= 0) {
         buttonDown.disabled = true;
     }
@@ -59,8 +67,11 @@ function SetFloor(newFloor){
     currentFloor = newFloor;
 
     floorSelect.value = newFloor;
+
+    //changes the backgroundimage to the image belonging to this floor.
     changeSvg(GetFloor(currentFloor).src);
 
+    //updates the url params with the current floor.
     updateUrlParams();
 
     //TODO: get data for this floor
